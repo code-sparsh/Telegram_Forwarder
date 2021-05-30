6335517fd22b9c8ae8b47cae9d54afed576a55da
code-sparsh <85060248+code-sparsh@users.noreply.github.com>
2021-05-30 16:39:41 +0530

    Create config.py

---
+++ forwarder/config.py
@@ -0,0 +1,9 @@
+class Development(Config):
+    API_KEY = "1867092778:AAFcMsbyqBpPRqWmySTvK1skEkFGGNyhGkM"  
+    OWNER_ID = 743659950 # Your user id
+
+   
+    FROM_CHATS = [-1276741264]  # List of chat id's to forward messages from.
+    TO_CHATS = [-1255626114]  # List of chat id's to forward messages to.
+    
+    WORKERS = 4
